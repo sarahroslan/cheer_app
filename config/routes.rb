5b309c20e70aa051b6057c1c6c6d13e26cce6502
Sarah <sarahroslan@gmail.com>
2018-08-13 13:03:35 +0800

    Fixed sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+ get 'sessions/new'
  root 'welcome#index'
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
 
  get "/dashboard" => "dashboard#index", as: "dashboard"
 
+ 
  
 end
